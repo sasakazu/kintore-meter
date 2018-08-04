9c5837031c5e487a26b56d0adb8823ce264d18c4
sasakazu <sasakura.company@gmail.com>
2018-08-04 18:11:19 +0900

    next todo ....

--- kin-meter/progressMenuViewController.swift
+++ kin-meter/progressMenuViewController.swift
@@ -13,7 +13,7 @@ class progressMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     
@@ -25,9 +25,11 @@ class progressMenuViewController: UIViewController {
     }
     
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
+        
     }
     
 
