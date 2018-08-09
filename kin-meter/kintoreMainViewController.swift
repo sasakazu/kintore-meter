0f7cdb56cc0525796fd0ac2ce1a3b7ffb2a7cc85
sasakazu <sasakura.company@gmail.com>
2018-08-09 20:30:59 +0900

    table move delete done.... move datasynclo...

--- kin-meter/kintoreMainViewController.swift
+++ kin-meter/kintoreMainViewController.swift
@@ -21,11 +21,14 @@ class kintoreMainViewController: UIViewController, UITableViewDataSource, UITabl
         mainTableView.delegate = self
         mainTableView.dataSource = self
         
+    
+        
         if UserDefaults.standard.object(forKey: "TodoList") != nil {
             TodoKobetsunonakami = UserDefaults.standard.object(forKey: "TodoList") as! [String]
         }
+
         
-        
+        navigationItem.leftBarButtonItem = editButtonItem
         
     }
     
@@ -35,19 +38,23 @@ class kintoreMainViewController: UIViewController, UITableViewDataSource, UITabl
     
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        
+
         if editingStyle == .delete {
-            
+
             TodoKobetsunonakami.remove(at: indexPath.row)
-            
+
             tableView.deleteRows(at: [indexPath as IndexPath], with: .fade)
-            
+        
+      
+
             let userDefaults = UserDefaults.standard
             userDefaults.set(TodoKobetsunonakami, forKey: "TodoList")
             userDefaults.synchronize()
-            
+
         }
     }
+    
+
    
     
     @IBAction func editBtn(_ sender: Any) {
@@ -75,13 +82,40 @@ class kintoreMainViewController: UIViewController, UITableViewDataSource, UITabl
     }
     
   
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // TODO: 入れ替え時の処理を実装する（データ制御など）
+        
+    }
+
+
+
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .delete
+    }
     
+    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
     
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        mainTableView.isEditing = editing
+    
+    }
+    
+    
+    
+    
     
 
 
